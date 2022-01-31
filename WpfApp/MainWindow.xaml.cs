25d531b007e9d35da4d75e1e139ea803bfc30903
andreymlv <ndrmlv@yandex.ru>
2022-01-31 17:29:00 +0300

    added simple wpf app

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
@@ -23,6 +11,11 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonMinutesToRead(object sender, RoutedEventArgs e)
+        {
+            tbResult.Text = StringUtils.StringUtils.MinutesToRead(textInput.Text, 200).ToString();
         }
     }
 }
