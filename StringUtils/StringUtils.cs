76412d0a3960eaeacf049101e95521a5819aee91
andreymlv <ndrmlv@yandex.ru>
2022-01-31 16:40:47 +0300

    first commit

---
+++ StringUtils/StringUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace StringUtils;
+
+public static class StringUtils
+{
+    /// <summary>
+    ///     Рассчитать время для чтения в минутах.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="wordsPerMinute"></param>
+    /// <returns></returns>
+    public static double MinutesToRead(string input, double wordsPerMinute = 150)
+    {
+        int words = input.Split(' ').Length;
+        return MinutesToRead(words, wordsPerMinute);
+    }
+
+    public static double MinutesToRead(int words, double wordsPerMinute = 150)
+    {
+        return words / wordsPerMinute;
+    }
+}
---
+++ Test/UnitTestStringUtils.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Test;
+
+public class UnitTestStringUtils
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(4, 2 + 2);
+    }
+}
